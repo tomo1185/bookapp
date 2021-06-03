8061bb53621cb590c4a27a4cdd3415a22105f139
tomo1185 <furarith.919biz@gmail.com>
2021-06-03 12:50:25 +0900

    config/app.phpの修正:
    timezone, localeを変更

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,4 +1,24 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -20,4 +40,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
